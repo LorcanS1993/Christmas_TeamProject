34f60fd669f711a19f72cd2ef526473834e66a86
MaghnusOD <g00377514@atu.ie>
2022-11-28 13:21:27 +0000

    set up pom file and added TicketInfoTest for unit testing

---
+++ src/test/java/ie/atu/TicketInfoTest.java
@@ -0,0 +1,38 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketInfoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void setUsr_name() {
+    }
+
+    @Test
+    void setUsr_occupation() {
+    }
+
+    @Test
+    void setUsr_age() {
+    }
+
+    @Test
+    void setUsr_card_num() {
+    }
+
+    @Test
+    void setUsr_ticket_num() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
