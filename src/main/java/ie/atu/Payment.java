5a7ea5d3681917f947cba35745dfc0657f605d17
niall.mcloughlin <G00362680@atu.ie>
2022-12-05 10:25:15 +0000

    added card number, card date and card CCV test

--- src/main/java/ie/atu/Payment.java
+++ src/main/java/ie/atu/Payment.java
@@ -4,4 +4,30 @@ public class Payment {
 String cardNumber;
 String cardDate;
 String cardCCV;
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardDate() {
+        return cardDate;
+    }
+
+    public void setCardDate(String cardDate) {
+        this.cardDate = cardDate;
+    }
+
+    public String getCardCCV() {
+        return cardCCV;
+    }
+
+    public void setCardCCV(String cardCCV) {
+        this.cardCCV = cardCCV;
+    }
 }
+
+
